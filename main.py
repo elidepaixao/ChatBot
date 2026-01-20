aa8341f00f15900289122f1cec1bff2d05ae217e
Elide <elidepaixao@hotmail.com>
2026-01-20 10:41:07 -0300

    Iniciando projeto

---
+++ main.py
@@ -0,0 +1,16 @@
+#Passo a passo do pensamento lógico para resolver o problema:
+# titulo
+# input do chat
+# a cada mensagem que o usuario enviar:
+  # mostrar a mensagem que o usuario enviou no chat
+  # pegar a pergunta e enviar para a IA responder
+  # exibir a resposta da IA no chat
+
+#para construir um chatbot vamos escolher os frameworks necessarios
+# Principais ferramentas de criacao de sistemas: Streamlit, Flask, Django, FastAPI
+
+# STREAMLIT -> permite crair o frintend e o backend utilizando apenas python
+# IA que vamos utilizar: OpenAI
+
+# instalando o Streamlit e OpenAI:
+
